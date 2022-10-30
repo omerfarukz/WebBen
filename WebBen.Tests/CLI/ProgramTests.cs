fff02691dce1f2fa4f9d182c09571ca759cb3cf0
omerfarukz <ulrozremo@gmail.com>
2022-10-30 11:39:45 +0300

    Add test to Program.cs

--- WebBen.Tests/CLI/ProgramTests.cs
+++ WebBen.Tests/CLI/ProgramTests.cs
@@ -1,19 +1,15 @@
-using System.CommandLine.Invocation;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using WebBen.CLI;
-using WebBen.Core.Logging;
 
 namespace WebBen.Tests.CLI;
 
-public class ProgramTests : TextWriterTestBase
+public class ProgramTests
 {
     [Test]
     public async Task AnalyzeCommand_Should_Parse_Args()
     {
-        var logger = new TextWriterLogger(StreamWriter);
         await Program.Main(new[] {"--help"});
-
         Assert.Pass();
     }
 }
